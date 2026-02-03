0eb4a0bf052fa5049c91dad1730a55886053ef09
kría <kria@kria.se>
2026-02-03 08:27:06 +0000

    fix: previous fix of git not existing in docker was foolish so we'll try something proper

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node
 WORKDIR /app
+ARG GIT_COMMIT
+ENV GIT_COMMIT=$GIT_COMMIT
 COPY package.json /app
 RUN npm install
 COPY . /app
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import { randomUUID } from "crypto";
 import express, { Request, Response } from "express";
 import rateLimit from "express-rate-limit";
-import { readFileSync } from "fs";
-import { join } from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -78,14 +76,8 @@ app.use((req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    try {
-        const hashfile_content = readFileSync(join(__dirname, "GIT_HASH"), { encoding: "utf-8" });
-        process.env.GIT_REVISION_HASH = hashfile_content.replace(/\n/g, "");
-    } catch (e) {
-        console.error("Could not set environment variable GIT_REVISION_HASH, encountered error:", e);
-
-        process.env.GIT_REVISION_HASH = undefined;
-    }
+    console.log("Version:");
+    console.log(process.env.GIT_COMMIT);
 
     console.log(`Server running at http://0.0.0.0:${port}`);
 });
